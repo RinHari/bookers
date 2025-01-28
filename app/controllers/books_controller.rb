91e3e61cc73a96b5b487dee3fc528245ae3231fc
EC2 Default User <ec2-user@ip-172-31-15-219.ap-northeast-1.compute.internal>
2025-01-28 07:21:48 +0000

    kadai

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+  def create
+    # １. データを受け取り新規登録するためのインスタンス作成
+    @new_book = Book.new(book_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    if @new_book.save
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@new_book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      @book = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.all
+    @new_book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params) 
+      flash[:notice] = "更新に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "更新に失敗しました。" 
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books/'  # 投稿一覧画面へリダイレクト  
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
